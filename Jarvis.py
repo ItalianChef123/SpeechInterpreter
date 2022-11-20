907952d079679d236cfef2ca9c75e114ac3fe08b
ItalianChef123 <Ollie-Thornton@hotmail.com>
2022-11-20 14:11:36 +0000

    Added weather predictor

--- Jarvis.py
+++ Jarvis.py
@@ -6,6 +6,7 @@ import cv2
 import pytesseract
 import requests
 import winsound
+
 pytesseract.pytesseract.tesseract_cmd = "C:/Users/Ollie/AppData/Local/Programs/Tesseract-OCR/tesseract.exe"
 now = datetime.now()
 dt_string = now.strftime("%d/%m/%Y %H:%M ")
@@ -16,10 +17,8 @@ engine.setProperty('rate', 150)
 voices = engine.getProperty('voices')
 engine.setProperty("voice", voices[1].id)
 with mic as source:
+    winsound.PlaySound("IronMan.wav", winsound.SND_ASYNC)
     while True:
-        winsound.PlaySound("IronMan.wav", winsound.SND_ASYNC)
-        audio = r.listen(source)
-
         while True:
             try:
                 audio = r.listen(source)
@@ -29,7 +28,6 @@ with mic as source:
             except sr.UnknownValueError:
                 engine.say("Say activate to begin")
                 engine.runAndWait()
-                print("test")
             except KeyboardInterrupt:
                 raise
         if recognized == "activate":
@@ -43,7 +41,7 @@ with mic as source:
                     engine.runAndWait()
                     recognized = ""
                 elif recognized == "what's the time":
-                    engine.say("It is "+dt_string)
+                    engine.say("It is " + dt_string)
                     engine.runAndWait()
                     recognized = ""
                 elif recognized == "stop":
@@ -53,6 +51,7 @@ with mic as source:
                 elif recognized == "thank you":
                     engine.say("You're welcome")
                     engine.runAndWait()
+                    recognized = ""
                 elif recognized == "dictate":
                     engine.say("What is the file called")
                     engine.runAndWait()
@@ -90,15 +89,16 @@ with mic as source:
                     engine.say(text)
                     engine.runAndWait()
                     print(text)
+                    recognized = ""
                 elif recognized == "weather":
                     headers = {
                         "Accept": "application/json"
                     }
 
                     coat = 0
-                    r = requests.get(
-                        'https://api.open-meteo.com/v1/forecast?latitude=51.8004&longitude=-0.2169&hourly=precipitation',
-                        params={}, headers=headers).json()
+                    r2 = requests.get(
+                        'https://api.open-meteo.com/v1/forecast?latitude=51.8004&longitude='
+                        '-0.2169&hourly=precipitation', params={}, headers=headers).json()
                     if datetime.now().day < 10 and datetime.now().month < 10:
                         day = str(datetime.now().year) + "-0" + str(datetime.now().month) + "-0" + str(
                             datetime.now().day) + "T"
@@ -111,20 +111,33 @@ with mic as source:
                     else:
                         day = str(datetime.now().year) + "-" + str(datetime.now().month) + "-" + str(
                             datetime.now().day) + "T"
-                    r_time = r["hourly"]["time"]
-                    r_precipitation = r["hourly"]["precipitation"]
+                    r_time = r2["hourly"]["time"]
+                    r_precipitation = r2["hourly"]["precipitation"]
 
-                    engine.say("What time are you going out(in the 24 hour format, for example 06:00)?")
-                    outing_time = input("What time are you going out(in the 24 hour format, for example 06:00)? ")
-                    outing_length = int(input("How many hours will you be out (if not a full hour round up)"))
+                    engine.say("What time are you going out(in the 24 hour format and hourly intervals"
+                                " for example 06:00)?")
+                    engine.runAndWait()
+                    outing_time_audio = r.listen(source)
+                    outing_time = r.recognize_google(outing_time_audio)
+                    if int(outing_time) < 10:
+                        outing_time = "0" + outing_time
+                    outing_time = outing_time + ":00"
+                    engine.say("How many hours will you be out (if not a full hour round up)")
+                    engine.runAndWait()
+                    outing_length_audio = r.listen(source)
+                    outing_length = r.recognize_google(outing_length_audio)
+                    outing_length = int(outing_length)
                     index = r_time.index(day + outing_time)
                     for i in range(outing_length):
                         if r_precipitation[index + i] > 0:
                             coat = coat + 1
                     if coat > 0:
-                        print("You need to bring your coat (this is just a prediction)")
+                        engine.say("You need to bring your coat (this is just a prediction)")
+                        engine.runAndWait()
                     else:
-                        print("You do not need to bring your coat (this is just a prediction)")
+                        engine.say("You do not need to bring your coat (this is just a prediction)")
+                        engine.runAndWait()
+                    recognized = ""
                 else:
                     engine.say("I did not understand that")
                     engine.runAndWait()
